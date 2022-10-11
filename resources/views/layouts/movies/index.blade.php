37f69bf6e5661dc75d928d041ac015aa0a6d2ed7
Smurrowz <nigeldn1995@gmail.com>
2022-10-11 16:06:22 +0200

    added some  style

--- resources/views/layouts/movies/index.blade.php
+++ resources/views/layouts/movies/index.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
+<div class="movies">
+  <h1 class="title">Movies:</h1>
   <div class="card-container">
     @foreach($movies as $movie) 
     <div class="card">
-      <h3>Titolo : {{$movie->title}}</h3>
-      <p>Titolo Originale : {{$movie->original_title}}</p>
-      <p>Nazionalita' : {{$movie->nationality}}</p>
-      <span>Data di uscita : {{$movie->date}}</span>
-      <span>Voto : {{$movie->vote}}</span>
+      <h2>{{$movie->title}}</h2>
+      <p> <span class="card-text"> Titolo Originale : </span>{{$movie->original_title}}</p>
+      <p><span class="card-text">Nazionalita' : </span>{{$movie->nationality}}</p>
+      <p><span class="card-text">Data di uscita : </span>{{$movie->date}}</p>
+      <p><span class="card-text">Voto : </span>{{$movie->vote}}</p>
     </div>
     @endforeach
 
