8a3b68b7692708dcddb23b04adf5ecba2778c789
Smurrowz <nigeldn1995@gmail.com>
2022-10-11 15:40:05 +0200

    done  some stuff

--- resources/views/layouts/movies/index.blade.php
+++ resources/views/layouts/movies/index.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 <div>
-  
-  @foreach($movies as $movie) 
-  <div class="card">
-    <h3>Titolo : {{$movie->title}}</h3>
-    <p>Titolo Originale : {{$movie->original_title}}</p>
-    <p>Nazionalita' : {{$movie->nationality}}</p>
-    <span>Data di uscita : {{$movie->date}}</span>
-    <span>Voto : {{$movie->vote}}</span>
+  <div class="card-container">
+    @foreach($movies as $movie) 
+    <div class="card">
+      <h3>Titolo : {{$movie->title}}</h3>
+      <p>Titolo Originale : {{$movie->original_title}}</p>
+      <p>Nazionalita' : {{$movie->nationality}}</p>
+      <span>Data di uscita : {{$movie->date}}</span>
+      <span>Voto : {{$movie->vote}}</span>
+    </div>
+    @endforeach
+
   </div>
-  @endforeach
 
 </div>
 
